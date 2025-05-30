fee63afa212d51705ecd129154caeb30e76b3c70
Chokolate.exe <122819922+Devneto07@users.noreply.github.com>
2025-05-30 20:25:51 -0300

    Update

---
+++ src/modelo/Livro.java
@@ -0,0 +1,53 @@
+package modelo;
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private String editora;
+	private int anoPublicacao;
+	private int quantidadeExemplares;
+	private int ISBN;
+	
+	
+	//construtor
+	public Livro (String titulo, String autor, String editora, int anoPublicacao, int quantidadeExemplares, int ISBN) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.anoPublicacao = anoPublicacao;
+		this.quantidadeExemplares = quantidadeExemplares;
+		this.ISBN = ISBN;
+	}
+	
+	public void getTitulo() {
+		
+	}
+	
+	public void getAutor() {
+		
+	}
+	
+	public void getEditora() {
+		
+	}
+	
+	public void getConsultarPorISBN() {
+		
+	}
+	
+	public void getListarLivros() {
+		
+	}
+	
+	public void getBuscarPorAutor() {
+		
+	}
+	
+	public void setCadastrarLivro() {
+		
+	}
+	
+	public void setRemoverLivro() {
+		
+	}
+}
