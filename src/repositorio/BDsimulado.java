1d6a17fb577e700e3e648f157358c091cf1518b8
euzebio-chokolate <euzebioneto547@gmail.com>
2025-05-31 08:41:22 -0500

    Update

--- src/repositorio/BDsimulado.java
+++ src/repositorio/BDsimulado.java
@@ -1,5 +1,24 @@
 package repositorio;
 
-public class BDsimulado {
+import java.util.HashMap;
+import modelo.Livro;
+import modelo.Usuario;
+import modelo.Emprestimo;
 
+public class BDsimulado {
+	private static HashMap<String, Livro > livros = new HashMap<String, Livro>();
+	private static HashMap<String, Usuario> usuarios = new HashMap<String, Usuario>();
+	private static HashMap<String, Emprestimo> emprestimos = new HashMap<String, Emprestimo>();
+	
+	public static boolean addLivro() {
+		
+	}
+	
+	public static boolean removerLivro() {
+		
+	}
 }
+
+
+//removerLivro()
+//addLivro()
